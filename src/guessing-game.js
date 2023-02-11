c5c1c3df76d6c099946e23e49fc3ecef0d818be2
Ryhor Volasau <G.volosov@gmail.com>
2023-02-11 21:30:01 +0400

    feat: my solution

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,50 @@
+function setRange(min, max, xxx) {
+  let myNumber = Math.round(min + (max - min) / 2);
+
+  if (xxx < min || xxx > max) {
+    return `Иди ты в ..., сам одгадывай :) Нету этого числа в предложенном диапазоне`;
+  }
+  if (myNumber === xxx) {
+    return `Возьми с полки пирожок :) чемпион, число = ${xxx}`;
+  } else if (myNumber > xxx) {
+    return setRange(min, myNumber, xxx);
+  } else {
+    return setRange(myNumber, max, xxx);
+  }
+}
+
+console.log(setRange(0, 4048, 409));
+console.log(setRange(-354, 73, -36));
+console.log(setRange(10, 124, 1));
+
 class GuessingGame {
-    constructor() {}
+  constructor() {}
 
-    setRange(min, max) {
+  setRange(min, max) {
+    GuessingGame.last = max;
+    GuessingGame.first = min;
+    GuessingGame.myNumber = Math.round((min + (max - min)) / 2);
+  }
 
-    }
+  guess() {
+    GuessingGame.myNumber;
+    // console.log(GuessingGame.myNumber);
+    return GuessingGame.myNumber;
+  }
 
-    guess() {
+  lower() {
+    GuessingGame.last = GuessingGame.myNumber;
+    GuessingGame.myNumber = Math.round(
+      GuessingGame.first + (GuessingGame.myNumber - GuessingGame.first) / 2
+    );
+  }
 
-    }
-
-    lower() {
-
-    }
-
-    greater() {
-
-    }
+  greater() {
+    GuessingGame.first = GuessingGame.myNumber;
+    GuessingGame.myNumber = Math.round(
+      GuessingGame.myNumber + (GuessingGame.last - GuessingGame.myNumber) / 2
+    );
+  }
 }
 
 module.exports = GuessingGame;
